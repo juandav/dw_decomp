9a5b48b745f5a1f3e91329654f0aeb3efdc3c2aa
Pekka Jylha-Ollila <pekka.jylha.ollila@gmail.com>
2026-09-25 19:12:41 +0300

    Enable SJIS encoding in source files

--- Makefile
+++ Makefile
@@ -21,7 +21,8 @@ SPLAT := $(PYTHON) -m splat split
 MWCCWRAP ?= bin/mwccwrap/mwccwrap.exe
 MWCCWRAP_FLAGS ?= -dll "bin/cc_mips/cc_mips_40.dll"
 MWCCWRAP_FLAGS += -O4 -sdata 8 -Werror -requireprotos -gccincludes \
-		  -lang c -Cpp_exceptions off -RTTI off
+		  -lang c -Cpp_exceptions off -RTTI off -multibyteaware \
+		  -codepage 932
 
 export MWCIncludes =
 
@@ -32,7 +33,8 @@ METROWRAP_FLAGS ?= --use-wibo --wibo-path $(WIBO)
 METROWRAP_FLAGS += --mwcc-path $(MWCCWRAP) --split-sections \
 		 --elf-flags 0x00001001 \
 		 --as-march r3000 \
-		 --macro-inc-path include/macro.inc
+		 --macro-inc-path include/macro.inc \
+		 --target-encoding windows-31j
 
 OBJDIFF ?= bin/objdiff-cli-linux-x86_64
 
--- tools/dl_deps.sh
+++ tools/dl_deps.sh
@@ -7,17 +7,17 @@ BIN_DIR="$TOP/bin"
 
 mkdir -p "$BIN_DIR"
 
-rm -rf "$BIN_DIR/mwccwrap.zip" "$BIN_DIR/mwccwrap"
-wget -O "$BIN_DIR/mwccwrap.zip" https://github.com/ThirstyWraith/mwccwrap/releases/download/v1.0-custom.701b7a2/mwccwrap.zip
-sha256sum -c "$BIN_DIR/mwccwrap.zip.sha256"
-unzip -d "$BIN_DIR/mwccwrap" "$BIN_DIR/mwccwrap.zip"
-rm -rf "$BIN_DIR/mwccwrap.zip"
+rm -rf "$BIN_DIR/mwccwrap-v1.1-custom.1c58d68.zip" "$BIN_DIR/mwccwrap"
+wget -O "$BIN_DIR/mwccwrap-v1.1-custom.1c58d68.zip" https://github.com/ThirstyWraith/mwccwrap/releases/download/v1.1-custom.1c58d68/mwccwrap-v1.1-custom.1c58d68.zip
+sha256sum -c "$BIN_DIR/mwccwrap-v1.1-custom.1c58d68.zip.sha256"
+unzip -d "$BIN_DIR/mwccwrap" "$BIN_DIR/mwccwrap-v1.1-custom.1c58d68.zip"
+rm -rf "$BIN_DIR/mwccwrap-v1.1-custom.1c58d68.zip"
 
-rm -rf "$BIN_DIR/metrowrap-v0.1.3-custom.c0052d4-linux-x86_64.zip" "$BIN_DIR/metrowrap"
-wget -O "$BIN_DIR/metrowrap-v0.1.3-custom.c0052d4-linux-x86_64.zip" https://github.com/ThirstyWraith/metrowrap/releases/download/v0.1.3-custom.c0052d4/metrowrap-v0.1.3-custom.c0052d4-linux-x86_64.zip
-sha256sum -c "$BIN_DIR/metrowrap-v0.1.3-custom.c0052d4-linux-x86_64.zip.sha256"
-unzip -d "$BIN_DIR/metrowrap" "$BIN_DIR/metrowrap-v0.1.3-custom.c0052d4-linux-x86_64.zip"
-rm -rf "$BIN_DIR/metrowrap-v0.1.3-custom.c0052d4-linux-x86_64.zip"
+rm -rf "$BIN_DIR/metrowrap-v0.1.3-custom.a19d8f6-linux-x86_64.zip" "$BIN_DIR/metrowrap"
+wget -O "$BIN_DIR/metrowrap-v0.1.3-custom.a19d8f6-linux-x86_64.zip" https://github.com/ThirstyWraith/metrowrap/releases/download/v0.1.3-custom.a19d8f6/metrowrap-v0.1.3-custom.a19d8f6-linux-x86_64.zip
+sha256sum -c "$BIN_DIR/metrowrap-v0.1.3-custom.a19d8f6-linux-x86_64.zip.sha256"
+unzip -d "$BIN_DIR/metrowrap" "$BIN_DIR/metrowrap-v0.1.3-custom.a19d8f6-linux-x86_64.zip"
+rm -rf "$BIN_DIR/metrowrap-v0.1.3-custom.a19d8f6-linux-x86_64.zip"
 
 rm -rf "$BIN_DIR/mkpsxiso-2.20-Linux.zip" "$BIN_DIR/mkpsxiso-2.20-Linux"
 wget -O "$BIN_DIR/mkpsxiso-2.20-Linux.zip" https://github.com/Lameguy64/mkpsxiso/releases/download/v2.20/mkpsxiso-2.20-Linux.zip
@@ -30,9 +30,12 @@ wget -O "$BIN_DIR/objdiff-cli-linux-x86_64" https://github.com/encounter/objdiff
 sha256sum -c "$BIN_DIR/objdiff-cli-linux-x86_64.sha256"
 chmod a+x "$BIN_DIR/objdiff-cli-linux-x86_64"
 
-rm -rf "$BIN_DIR/wibo-x86_64" "$BIN_DIR/wibo-i686"
+rm -rf "$BIN_DIR/wibo-x86_64"
 wget -O "$BIN_DIR/wibo-x86_64" https://github.com/decompals/wibo/releases/download/1.1.0/wibo-x86_64
 sha256sum -c "$BIN_DIR/wibo-x86_64.sha256"
+chmod a+x "$BIN_DIR/wibo-x86_64"
+
+rm -rf "$BIN_DIR/wibo-i686"
 wget -O "$BIN_DIR/wibo-i686" https://github.com/decompals/wibo/releases/download/1.2.0/wibo-i686
 sha256sum -c "$BIN_DIR/wibo-i686.sha256"
-chmod a+x "$BIN_DIR/wibo-x86_64" "$BIN_DIR/wibo-i686"
+chmod a+x "$BIN_DIR/wibo-i686"
